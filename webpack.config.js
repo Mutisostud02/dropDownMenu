e1672435cada22c103a27a5e9dc802e8c82a9e14
Mutisostud02 <107198709+Mutisostud02@users.noreply.github.com>
2023-12-28 19:01:48 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    home: "./src/home.js",
+    menu: "./src/menu.js",
+    contacts: "./src/contacts.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+};
